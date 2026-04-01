667ec0143b2fa1f81be8322810e16f6a119c42cf
Samuel Ray <raysj@vcu.edu>
2026-03-31 20:45:49 -0400

    Initial Commit

---
+++ FFConvert/FFConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FFConvertApp.swift
+//  FFConvert
+//
+//  Created by Samuel Ray on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct FFConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
